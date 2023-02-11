05abda68ce6ed29cdd58241ad0d86df77559c02e
Faayiz <faayizm98@gmail.com>
2023-02-11 13:56:55 +0530

    profile name change for the netlify deploy check

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,17 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Faayiz </h2>;
+  const three = <h3 className="big-heading">I build things for embedded systems and IoT.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
+        I’m a software engineer specializing in building embedded systems, linux device driver development
+        and low level controller development. I am focused on creating end to end system from device driver to
+	application programs
         at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        <a href="https://symphony.rakuten.com/" target="_blank" rel="noreferrer">
+          Rakuten Symphony
         </a>
         .
       </p>
@@ -81,7 +82,7 @@ const Hero = () => {
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Get in touch
     </a>
   );
 
